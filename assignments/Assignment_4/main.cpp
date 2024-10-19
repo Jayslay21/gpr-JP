6aeba219e414946f79713f645e6a110f47eb9b47
Jayslay21 <jonathan.pacheco@mymail.champlain.edu>
2024-10-19 12:32:47 -0400

    Added assignment 4 subdirectory

---
+++ assignments/Assignment_4/main.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+// Vertex and Fragment Shader source code
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
+    layout(location = 1) in vec4 aColor;
+    out vec4 ourColor;
+    uniform vec2 uOffset; // Uniform for movement offset
+    void main() {
+        gl_Position = vec4(aPos + vec3(uOffset, 0.0), 1.0);
+        ourColor = aColor;
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    in vec4 ourColor;
+    out vec4 FragColor;
+    uniform float uTime;
+    void main() {
+        float greenValue = (sin(uTime) / 2.0f) + 0.5f;
+        FragColor = vec4(ourColor.r, greenValue, ourColor.b, ourColor.a);
+    }
+)";
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+};
+
+unsigned int compileShader(unsigned int type, const char* source) {
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::COMPILATION_FAILED\n%s", infoLog);
+    }
+    return shader;
+}
+
+unsigned int setupShaders() {
+    unsigned int vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::PROGRAM::LINKING_FAILED\n%s", infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+unsigned int setupVertexArrayObject() {
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    return VAO;
+}
+
+int main() {
+    printf("Initializing...\n");
+
+    if (!glfwInit()) {
+        printf("GLFW failed to init!\n");
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    if (!window) {
+        printf("GLFW failed to create window\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        printf("GLAD Failed to load GL headers");
+        return 1;
+    }
+
+    unsigned int shaderProgram = setupShaders();
+    unsigned int VAO = setupVertexArrayObject();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+      
+        glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+       
+        glUseProgram(shaderProgram);
+        float time = (float)glfwGetTime();
+        int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
+        glUniform1f(timeLoc, time);
+
+        
+        float offsetX = sin(time) * 0.1f;
+        float offsetY = cos(time) * 0.1f;
+        int offsetLoc = glGetUniformLocation(shaderProgram, "uOffset");
+        glUniform2f(offsetLoc, offsetX, offsetY);
+
+        
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+    }
+
+    printf("Shutting down...\n");
+    glfwTerminate();
+    return 0;
+}
