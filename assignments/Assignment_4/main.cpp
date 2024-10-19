00e4cbf8403390a67e6461350640f6aabf70bf6d
Jayslay21 <jonathan.pacheco@mymail.champlain.edu>
2024-10-19 13:06:36 -0400

    Changed shape to square and added rotation 3D

--- assignments/Assignment_4/main.cpp
+++ assignments/Assignment_4/main.cpp
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
-// Vertex and Fragment Shader source code
 const char* vertexShaderSource = R"(
     #version 330 core
     layout(location = 0) in vec3 aPos;
     layout(location = 1) in vec4 aColor;
     out vec4 ourColor;
-    
-    uniform vec2 uOffset; // Uniform for movement offset
-    uniform mat4 transform; // Uniform for matrix transformation
-    
+
+    uniform vec2 uOffset; 
+    uniform mat4 transform; 
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+
     void main() {
-        gl_Position = transform * vec4(aPos + vec3(uOffset, 0.0), 1.0);
+        gl_Position = projection * view * model * transform * vec4(aPos + vec3(uOffset, 0.0), 1.0);
         ourColor = aColor;
     }
 )";
@@ -38,9 +41,15 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+    // First triangle
+    -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  // Top-left
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // Bottom-left
+     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // Bottom-right
+
+     // Second triangle
+      0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // Bottom-right
+      0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,  // Top-right
+     -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f   // Top-left
 };
 
 unsigned int compileShader(unsigned int type, const char* source) {
@@ -108,7 +117,7 @@ int main() {
         return 1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Square", NULL, NULL);
     if (!window) {
         printf("GLFW failed to create window\n");
         glfwTerminate();
@@ -124,6 +133,9 @@ int main() {
     unsigned int shaderProgram = setupShaders();
     unsigned int VAO = setupVertexArrayObject();
 
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
@@ -131,27 +143,32 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+
         float time = (float)glfwGetTime();
         int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
         glUniform1f(timeLoc, time);
 
-        // Apply offset based on time
         float offsetX = sin(time) * 0.1f;
         float offsetY = cos(time) * 0.1f;
         int offsetLoc = glGetUniformLocation(shaderProgram, "uOffset");
         glUniform2f(offsetLoc, offsetX, offsetY);
 
-        // Apply transformation matrix (rotation + scaling)
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
-
+        glm::mat4 trans = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
         int transformLoc = glGetUniformLocation(shaderProgram, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
     }
